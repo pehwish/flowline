a4f08545f7888a5f53193fc547d24ef2b9839dbc
박은혜 <pehwish@gmail.com>
2023-12-18 23:25:57 +0900

    * 스크롤 추천  css 추가

--- js/script.js
+++ js/script.js
@@ -2,16 +2,13 @@ import works from './work.js';
 
 const lenis = new Lenis();
 
-lenis.on('scroll', e => {
-  console.log(e);
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add(time => {
+  lenis.raf(time * 1000);
 });
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
+gsap.ticker.lagSmoothing(0);
 
 function init() {
   gsap.registerPlugin(ScrollTrigger);
